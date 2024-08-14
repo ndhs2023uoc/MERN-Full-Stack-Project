8f3839fd7b5b35c523d8c0e54efb9f8e967b5731
ndhs2023uoc <nadun202327@gmail.com>
2024-08-14 09:03:44 +0530

    initial commit-creating backend for this project

---
+++ backend/index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//mongodb connection
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mern-project.okzqd.mongodb.net/?retryWrites=true&w=majority&appName=mern-project`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // create a database and collections
+    const database = client.db("mern-project");
+    const usersCollections = database.collection("users");
+    const classesCollections = database.collection("classes");
+    const cartCollections = database.collection("cart");
+    const paymentCollection = database.collection("payments");
+    const enrolledCollection = database.collection("enrolled");
+    const appliedCollection = database.collection("applied");
+
+    // classes routes here
+    app.post("/new-class", (req, res) => {});
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
